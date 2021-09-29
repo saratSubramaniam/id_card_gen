9aba596fe98c811266ef30cb83152eff9fc01ed2
Sarath Subramaniam <sarath.s@10xds.com>
2021-09-29 16:35:00 +0530

    sdsd

--- index2.js
+++ index2.js
@@ -3,24 +3,22 @@ var fs = require("fs");
 const cors = require('cors');
 
 var express = require('express');
-var router = express.Router();
-
 var app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
 
-// var server = app.listen(8081, function () {
-//     var host = server.address().address;
-//     var port = server.address().port;
-//     console.log("Example app listening at http://%s:%s", host, port);
-// });
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+});
 
-router.get('/', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-router.post('/send', cors(), function (req, res) {
+app.post('/send', cors(), function (req, res) {
     let content = fs.readFileSync("template.html", "utf8");
     // console.log(req.body);
 
