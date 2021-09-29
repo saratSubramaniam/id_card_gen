c95d114e4ec836f6f9248ac1a3aa0a6a497b996e
Sarath Subramaniam <sarath.s@10xds.com>
2021-09-29 17:22:52 +0530

    sd

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ var fs = require("fs");
 const cors = require('cors');
 
 var express = require('express');
-var router = express.Router();
+var app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
-// var app = express();
-// app.use(cors());
-// app.use(express.json({ limit: '50mb' }));
-// app.use(express.urlencoded({ limit: '50mb' }));
+const port = process.env.port || 8080;
 
-// var server = app.listen(8081, function () {
-//     var host = server.address().address;
-//     var port = server.address().port;
-//     console.log("Example app listening at http://%s:%s", host, port);
-// });
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+});
 
-router.get('/', (req, res) => {
-    res.write('Hello World!');
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
-router.post('/send', cors(), function (req, res) {
+app.post('/send', cors(), function (req, res) {
     let content = fs.readFileSync("template.html", "utf8");
     // console.log(req.body);
 
@@ -48,4 +48,8 @@ router.post('/send', cors(), function (req, res) {
     });
 });
 
-module.exports = router;
+app.post('/send', function (req, res) {
+    console.log(req);
+
+    res.end();
+});
--- index1.js
+++ index1.js
@@ -3,22 +3,24 @@ var fs = require("fs");
 const cors = require('cors');
 
 var express = require('express');
-var app = express();
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+var router = express.Router();
 
-var server = app.listen(8081, function () {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Example app listening at http://%s:%s", host, port);
+// var app = express();
+// app.use(cors());
+// app.use(express.json({ limit: '50mb' }));
+// app.use(express.urlencoded({ limit: '50mb' }));
+
+// var server = app.listen(8081, function () {
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("Example app listening at http://%s:%s", host, port);
+// });
+
+router.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.post('/send', cors(), function (req, res) {
+router.post('/send', cors(), function (req, res) {
     let content = fs.readFileSync("template.html", "utf8");
     // console.log(req.body);
 
@@ -46,8 +48,4 @@ app.post('/send', cors(), function (req, res) {
     });
 });
 
-app.post('/send', function (req, res) {
-    console.log(req);
-
-    res.end();
-});
+module.exports = router;
